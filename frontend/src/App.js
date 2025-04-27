6e472f13deece7578668c4ea62c2fca4225d4b2d
praditha-b <pradithab7@gmail.com>
2025-04-27 22:21:12 +0530

    Initial frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import GoalList from './components/GoalList/GoalList'
+
+function App() {
+  return (
+    <div>
+   <h1>Course Goals</h1>
+   <GoalList/>
+   </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/GoalList/GoalList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const GoalList = () => {
+    return (
+        
+        <ul>
+        <li>Goal 1: </li>
+        <li>Goal 2: </li>
+        </ul>
+        
+    );
+}
+
+export default GoalList
