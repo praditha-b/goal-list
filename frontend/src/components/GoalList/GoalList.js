0a15746a8a884ea322ed6ca4240025e0bfc78d58
praditha-b <pradithab7@gmail.com>
2025-04-30 22:25:44 +0530

    added text form

--- frontend/src/components/GoalList/GoalList.js
+++ frontend/src/components/GoalList/GoalList.js
@@ -5,7 +5,11 @@ import './GoalList.css';
 const GoalList = props  => {
     console.log(props.goals)
     return (
-        <div>        
+        <div className='goalListContainer'>      
+        <form className='inputForm'> 
+            
+            <input className='inputGoal' type='text' placeholder='Enter Goal'></input>
+        </form>  
         <ul className='goalList'>
         {
             props.goals.map(goal => <li key={goal.id}>{goal.text}</li>)
